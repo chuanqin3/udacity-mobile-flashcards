fdd491806b4e5549c12437a9180e94a0ec234049
ChuanQin <callforsky@gmail.com>
2018-10-21 19:41:14 -0400

    start to make the quiz views

---
+++ screens/QuizScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native'
+import { connect } from 'react-redux'
+import TextButton from '../components/TextButton'
+import { addCard } from '../actions'
+import { NavigationActions } from 'react-navigation'
+
+class QuizScreen extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>Questions</Text>
+      </View>
+    )
+  }
+}
+
+export default connect()(QuizScreen)
